6c23fab2caf94d6ccbc90ec388073203641b08a8
ScottHull <scottdhull21@gmail.com>
2019-10-04 11:21:09 -0400

    Initial attempt at 182W and 184W partitioning.

--- model.py
+++ model.py
@@ -9,49 +9,14 @@ class Model:
             silicate_thermal_expansivity, gravitational_acceleration, silicate_heat_capacity
     ):
 
-        self.__half_life_182hf = 8.9 * 10**6  # the half life of 182Hf, do not set
-        self.__decay_const_182hf = log(0.5) / self.__half_life_182hf  # the decay constant of 182Hf, do not set
-        self.body_core_mass = body_core_mass  # the core mass of the modern body, set
-        self.body_mass = body_mass  # the bulk mass of the modern body, set
-        self.body_radius = body_radius
+        # general modeling parameters
         self.timestep = timestep  # the modeling timestep, set
         self.time = 0  # the model time, do not set
         self.max_time_core_formation = max_time_core_formation  # the maximum timing of core formation, set
-        self.fO2 = fO2
-
         self.__molar_mass_182w = 183.84
         self.__molar_mass_182hf = 178.49
-
-        self.core_mass = 0  # the modeled core mass at time, do not set
-        self.mantle_mass = 0
-        self.metal_mass_added = 0
-        self.core_fraction_per_timestep = core_fraction_per_timestep  # the fraction of metal to be added to the core, set
-
-        self.conc_bulk_182hf = conc_bulk_182hf
-        self.mass_bulk_182hf = self.conc_bulk_182hf * body_mass
-        self.moles_bulk_182hf = self.mass_bulk_182hf / self.__molar_mass_182hf
-        self.conc_silicate_182hf = 0
-        self.__original_moles_bulk_182hf = copy(self.moles_bulk_182hf)
-        self.__previous_timestep_moles_bulk_182hf = copy(self.moles_bulk_182hf)
-
-        self.conc_silicate_182w = 0
-        self.moles_bulk_182w = 0
-        self.mass_bulk_182w = 0
-        self.__conc_metal_182w = 0
-        self.conc_core_182w = 0
-
-        self.conc_bulk_184w = conc_bulk_184w
-        self.conc_silicate_184w = self.conc_bulk_184w
-        self.conc_core_184w = 0
-        self.conc_core_bound_metal_184w = 0
-        self.mass_core_bound_metal_184w = 0
-        self.conc_core_bound_metal_182w = 0
-        self.mass_core_bound_metal_182w = 0
-        self.mass_silicate_184w = (self.conc_silicate_184w * self.body_mass)
-        self.mass_core_184w = 0
-        self.mass_core_182w = 0
-        self.mass_silicate_182w = 0
-
+        self.__half_life_182hf = 8.9 * 10 ** 6  # the half life of 182Hf, do not set
+        self.__decay_const_182hf = log(0.5) / self.__half_life_182hf  # the decay constant of 182Hf, do not set
         self.alpha = alpha
         self.beta = beta
         self.chi = chi
@@ -69,10 +34,50 @@ class Model:
 
         self.core_mantle_boundary_depth = 0
 
-        self.volume_core = 0
-        self.volume_mantle = (4 / 3) * pi * (self.body_radius)**3
+        # environment parameters
+        self.fO2 = fO2
         self.density_metal = 7800
         self.density_silicate = 3750
+
+        # planet/planetesimal evolution parameters
+        self.body_core_mass = body_core_mass  # the core mass of the modern body, set
+        self.body_mass = body_mass  # the bulk mass of the modern body, set
+        self.body_radius = body_radius
+        self.core_mass = 0  # the modeled core mass at time, do not set
+        self.mantle_mass = 0
+        self.metal_mass_added = 0
+        self.core_fraction_per_timestep = core_fraction_per_timestep  # the fraction of metal to be added to the core
+        self.volume_core = 0
+        self.volume_mantle = (4 / 3) * pi * (self.body_radius) ** 3
+
+        # 182hf parameters
+        self.conc_bulk_182hf = conc_bulk_182hf
+        self.mass_bulk_182hf = self.conc_bulk_182hf * body_mass
+        self.moles_bulk_182hf = self.mass_bulk_182hf / self.__molar_mass_182hf
+        self.conc_silicate_182hf = 0
+        self.__original_moles_bulk_182hf = copy(self.moles_bulk_182hf)
+        self.__previous_timestep_moles_bulk_182hf = copy(self.moles_bulk_182hf)
+
+        # 182w parameters
+        self.conc_silicate_182w = 0
+        self.moles_bulk_182w = 0
+        self.mass_bulk_182w = 0
+        self.__conc_metal_182w = 0
+        self.conc_core_182w = 0
+        self.conc_core_bound_metal_182w = 0
+        self.mass_core_bound_metal_182w = 0
+        self.mass_core_182w = 0
+        self.mass_silicate_182w = 0
+
+        # 184w parameters
+
+        self.conc_bulk_184w = conc_bulk_184w
+        self.conc_silicate_184w = self.conc_bulk_184w
+        self.conc_core_184w = 0
+        self.conc_core_bound_metal_184w = 0
+        self.mass_core_bound_metal_184w = 0
+        self.mass_silicate_184w = (self.conc_silicate_184w * self.body_mass)
+        self.mass_core_184w = 0
 
     def adiabat(self):
 
@@ -96,26 +101,6 @@ class Model:
         D = 10**logD
 
         return D
-
-    def partition_184w(self, D):
-
-        self.conc_core_bound_metal_184w = self.conc_silicate_184w * D  # make sure conc silicate reflects equilibrium
-        self.mass_core_bound_metal_184w = self.conc_core_bound_metal_184w * self.metal_mass_added
-        self.mass_core_184w += self.mass_core_bound_metal_184w
-        self.mass_silicate_184w -= self.mass_core_bound_metal_184w
-        self.conc_core_184w = self.mass_core_184w / self.core_mass
-        self.conc_silicate_184w = self.mass_silicate_184w / self.mantle_mass
-        
-        
-    def partition_182w(self, D):
-
-        self.conc_core_bound_metal_182w = self.conc_silicate_182w
-        self.mass_core_bound_metal_182w = self.conc_core_bound_metal_182w * self.metal_mass_added
-        self.mass_core_182w = self.mass_core_bound_metal_182w
-        self.mass_silicate_184w -= self.mass_core_bound_metal_182w
-        self.conc_core_182w = self.mass_core_182w / self.core_mass
-        self.conc_silicate_182w = self.mass_silicate_182w / self.mantle_mass
-
 
     def fractionate_metal_exponential(self):
 
@@ -150,7 +135,29 @@ class Model:
         self.mass_bulk_182hf = (self.__original_moles_bulk_182hf * self.__molar_mass_182hf) - self.mass_bulk_182w
 
 
-    def set_concentrations(self):
+    def exchange_mass(self, D, conc_w_silicate):
+        """
+        The exchange mass of the either 182W or 184W prior to equilibration.  This function is generic in order to
+        support both 182W and 184W.
+        :param D:
+        :param conc_w_silicate:
+        :return:
+        """
 
-        self.conc_silicate_182hf = self.mass_bulk_182hf / self.mantle_mass
-        self.conc_bulk_182hf = self.conc_silicate_182hf
+        # conc_w_silicate should be the concentration of the isotope in the silicate at time t - \Delta t
+
+        numerator = conc_w_silicate / self.mantle_mass
+        denominator = ((1 / self.metal_mass_added) + (D * (1 / self.mantle_mass)))
+        m = D * (numerator / denominator)
+
+        return m
+
+    def set_concentrations(self, exchange_mass_182w, exchange_mass_184w):
+
+        # 182w
+        self.conc_silicate_182w = self.conc_silicate_182w - (exchange_mass_182w / self.mantle_mass)
+        self.conc_core_bound_metal_182w = exchange_mass_182w / self.metal_mass_added
+        
+        # 184w
+        self.conc_silicate_184w = self.conc_silicate_184w - (exchange_mass_184w / self.mantle_mass)
+        self.conc_core_bound_metal_184w = exchange_mass_184w / self.metal_mass_added
--- test.py
+++ test.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 density_silicate_melt = 3750
 density_metal = 7800
 max_core_formation_time = 5 * 10**6
-timestep = 1 * 10**6
+timestep = 5 * 10**6
 core_fraction_per_timestep = max_core_formation_time / timestep
 fO2 = -2.25
 body_mass = 2.59 * 10**20
@@ -16,7 +16,7 @@ conc_bulk_184w = 23.878 * 10**-9
 radius_vestian_core = 113 * 1000
 volume_vestian_core = (4 / 3) * pi * (radius_vestian_core**3)
 mass_vestian_core = volume_vestian_core * density_metal
-max_modeling_time = 3 * 10**6
+max_modeling_time = 24 * 10**6
 
 alpha = 1.11
 beta = -1.18
@@ -30,7 +30,7 @@ therm_expansivity = 6 * (10**(-5))
 heat_capacity = 10**3
 gravity = 0.25
 
-time_range = np.arange(0, max_modeling_time + timestep, timestep)
+time_range = []
 
 
 metal_added_at_time = []
@@ -74,10 +74,12 @@ m = Model(
     silicate_heat_capacity=heat_capacity,
 )
 
-while m.time <= max_modeling_time:
+while m.time < max_modeling_time:
 
     # advance the modeled time
     m.time += m.timestep
+    time_range.append(m.time)
+    print("At time {} Myr".format(m.time / 10**6))
 
     # decay 182Hf to 182W
     m.decay_182hf()
@@ -97,13 +99,19 @@ while m.time <= max_modeling_time:
     d = m.partitioning(temperature=t, pressure=p)
     cmb_depth.append(m.core_mantle_boundary_depth)
 
-    m.partition_184w(D=d)
-    core_conc_184w.append(m.conc_core_184w)
-    core_conc_182w.append(m.conc_core_182w)
-    core_mass_182w.append(m.mass_core_182w)
-    core_mass_184w.append(m.mass_core_184w)
-    mantle_mass_182w.append(m.mass_silicate_182w)
-    mantle_mass_184w.append(m.mass_silicate_184w)
+    if m.time <= m.max_time_core_formation:
+        # calculate equilibrium exchange
+        mass_exchange_182w = m.exchange_mass(D=d, conc_w_silicate=m.conc_silicate_182w)
+        mass_exchange_184w = m.exchange_mass(D=d, conc_w_silicate=m.conc_silicate_184w)
+    else:
+        mass_exchange_182w = 0
+        mass_exchange_184w = 0
+
+        # equilibrate
+    m.set_concentrations(exchange_mass_182w=mass_exchange_182w, exchange_mass_184w=mass_exchange_184w)
+    mass_182w_added_to_core = m.conc_core_bound_metal_182w * m.metal_mass_added
+    mass_184w_added_to_core = m.conc_core_bound_metal_184w * m.metal_mass_added
+    mass_182w_in_core = m.mass_core_182w +
 
 
 
@@ -117,15 +125,15 @@ ax1.set_xlabel("Time (Ma)")
 ax1.set_ylabel("Core Mass Added at Time (kg)")
 ax1.set_title("Exponential Core Growth Model on Vesta")
 
-fig2 = plt.figure()
-ax2 = fig2.add_subplot(111)
-ax2.plot([i / (10**6) for i in time_range], bulk_mass_182w, linewidth=1.5, color='black', label="182W Bulk Mass")
-ax2.plot([i / (10**6) for i in time_range], core_mass_182w, linewidth=1.5, color='red', label="182W Core Mass")
-ax2.plot([i / (10**6) for i in time_range], mantle_mass_182w, linewidth=1.5, color='blue', label="182W Mantle Mass")
-ax2.grid()
-ax2.set_xlabel("Time (Ma)")
-ax2.set_ylabel("Bulk Mass 182W (kg)")
-ax2.set_title("Bulk Mass 182W in Vesta")
+# fig2 = plt.figure()
+# ax2 = fig2.add_subplot(111)
+# ax2.plot([i / (10**6) for i in time_range], bulk_mass_182w, linewidth=1.5, color='black', label="182W Bulk Mass")
+# ax2.plot([i / (10**6) for i in time_range], core_mass_182w, linewidth=1.5, color='red', label="182W Core Mass")
+# ax2.plot([i / (10**6) for i in time_range], mantle_mass_182w, linewidth=1.5, color='blue', label="182W Mantle Mass")
+# ax2.grid()
+# ax2.set_xlabel("Time (Ma)")
+# ax2.set_ylabel("Bulk Mass 182W (kg)")
+# ax2.set_title("Bulk Mass 182W in Vesta")
 
 fig3 = plt.figure()
 ax3 = fig3.add_subplot(111)
@@ -147,23 +155,23 @@ ax4.set_ylabel("CMB Depth (km)")
 ax4.set_title("CMB Depth in Vesta")
 ax4.legend(loc='lower right')
 
-fig5 = plt.figure()
-ax5 = fig5.add_subplot(111)
-ax5.plot([i / (10**6) for i in time_range], [i * 10**9 for i in core_conc_184w], linewidth=1.5, color='blue', label="184W")
-ax5.plot([i / (10**6) for i in time_range], [i * 10**9 for i in core_conc_182w], linewidth=1.5, color='red', label="182W")
-ax5.grid()
-ax5.set_xlabel("Time (Ma)")
-ax5.set_ylabel("W Isotope Concentration (ppb)")
-ax5.set_title("W Isotope Concentration in Vesta")
-ax5.legend(loc='lower right')
-
-fig6 = plt.figure()
-ax6 = fig6.add_subplot(111)
-ax6.axhline(mass_vestian_core, linestyle="--", linewidth=1.0, color='red', label="Modern Vesta Core Mass")
-ax6.plot([i / (10**6) for i in time_range], core_mass, linewidth=1.5, color='black', label="Vestian Core Mass")
-ax6.grid()
-ax6.set_xlabel("Time (Ma)")
-ax6.set_ylabel("Core Mass (kg)")
-ax6.set_title("Exponential Core Growth Model on Vesta")
+# fig5 = plt.figure()
+# ax5 = fig5.add_subplot(111)
+# ax5.plot([i / (10**6) for i in time_range], [i * 10**9 for i in core_conc_184w], linewidth=1.5, color='blue', label="184W")
+# ax5.plot([i / (10**6) for i in time_range], [i * 10**9 for i in core_conc_182w], linewidth=1.5, color='red', label="182W")
+# ax5.grid()
+# ax5.set_xlabel("Time (Ma)")
+# ax5.set_ylabel("W Isotope Concentration (ppb)")
+# ax5.set_title("W Isotope Concentration in Vesta")
+# ax5.legend(loc='lower right')
+#
+# fig6 = plt.figure()
+# ax6 = fig6.add_subplot(111)
+# ax6.axhline(mass_vestian_core, linestyle="--", linewidth=1.0, color='red', label="Modern Vesta Core Mass")
+# ax6.plot([i / (10**6) for i in time_range], core_mass, linewidth=1.5, color='black', label="Vestian Core Mass")
+# ax6.grid()
+# ax6.set_xlabel("Time (Ma)")
+# ax6.set_ylabel("Core Mass (kg)")
+# ax6.set_title("Exponential Core Growth Model on Vesta")
 
 plt.show()
